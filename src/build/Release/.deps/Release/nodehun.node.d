707ced8d825970d93db314926c17e421ba1a61c7
Nathan Sweet <nathanjsweet@gmail.com>
2012-09-14 15:53:23 -0700

    removing build from git

--- src/build/Release/.deps/Release/nodehun.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/nodehun.node := ./gyp-mac-tool flock ./Release/linker.lock g++ -shared -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release -install_name /usr/local/lib/nodehun.node  -o Release/nodehun.node Release/obj.target/nodehun/nodehun.o Release/hunspell.node -undefined dynamic_lookup
