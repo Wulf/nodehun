85cbf3b96ca6ea44fbf1e382d98b07d360e5dcff
Nathan Sweet <nathanjsweet@gmail.com>
2013-04-22 16:58:09 -0700

    Test reflects passing raw string at initialization.

--- tests/a_init.js
+++ tests/a_init.js
@@ -1,10 +1,15 @@
 var nodehun = require('./../lib/index.js'),
-	Dictionary = nodehun.Dictionary,
-	mctime = require('microtime'),
-	unit = typeof mctime !== "undefined" ? "μs":"ms",
-	time = typeof mctime !== "undefined" ? mctime.now : Date.now;
+Dictionary = nodehun.Dictionary;
+var fs = require('fs'),
+mctime = require('microtime'),
+unit = typeof mctime !== "undefined" ? "μs":"ms",
+time = typeof mctime !== "undefined" ? mctime.now : Date.now;
+
+var affbuf = fs.readFileSync(__dirname+'/../dictionaries/en_US/en_US.aff').toString();
+var dictbuf = fs.readFileSync(__dirname+'/../dictionaries/en_US/en_US.dic').toString();
 var timeInit = time();
-var dict = new Dictionary('en_US');
+//var dict = new Dictionary('en_US');
+var dict = new Dictionary(affbuf,dictbuf);
 console.log('time to initialize dictionary class:',time() - timeInit,unit);
 timeInit = time();
 dict.spellSuggest('Tacoma',function(a,b){console.log('misspelling "Tacoma"',a,b,'time to receive result:',time() - timeInit,unit)});
