19cacf2d539a8d51257709b8d5024a8479d2aa38
Nathan Sweet <nathanjsweet@gmail.com>
2013-04-22 18:00:40 -0700

    modified tests to reflect the various modes of initialization

--- tests/a_init.js
+++ tests/a_init.js
@@ -7,11 +7,22 @@ time = typeof mctime !== "undefined" ? mctime.now : Date.now;
 
 var affbuf = fs.readFileSync(__dirname+'/../dictionaries/en_US/en_US.aff').toString();
 var dictbuf = fs.readFileSync(__dirname+'/../dictionaries/en_US/en_US.dic').toString();
+
+
+
 var timeInit = time();
-//var dict = new Dictionary('en_US');
 var dict = new Dictionary(affbuf,dictbuf);
 console.log('time to initialize dictionary class:',time() - timeInit,unit);
+dict.spellSuggest('Tacoma',function(a,b){console.log('misspelling "Tacoma"',a,b,'time to receive result:',time() - timeInit,unit)});
+dict.spellSuggestions('calor',function(a,b){console.log('misspelling "calor"',a,b)});
+
+dict.spellSuggest('color',function(a,b){console.log('correct "color"',a,b)});
+dict.spellSuggestions('color',function(a,b){console.log('correct "color"',a,b)});
+
 timeInit = time();
+dict = new Dictionary('en_US');
+console.log('time to initialize dictionary class:',time() - timeInit,unit);
+
 dict.spellSuggest('Tacoma',function(a,b){console.log('misspelling "Tacoma"',a,b,'time to receive result:',time() - timeInit,unit)});
 dict.spellSuggestions('calor',function(a,b){console.log('misspelling "calor"',a,b)});
 
