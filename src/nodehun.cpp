710eec02bd9dd146773a06a56f14c90789af7d84
Nathan Sweet <nathanjsweet@gmail.com>
2016-02-27 15:31:58 -0600

    adding warning message about no maintainers, bumping version

--- src/nodehun.cpp
+++ src/nodehun.cpp
@@ -1,4 +1,9 @@
 #include "license.nodehun"
+#ifdef _WIN32
+#pragma message("Nodehun is no longer being actively maintained. Please email nathanjsweet@gmail.com If you are interested in taking over the project.")
+#else
+#warning "Nodehun is no longer being actively maintained. Please email nathanjsweet@gmail.com If you are interested in taking over the project."
+#endif
 #include <node_version.h>
 #define NODE_0_12_MODULE_VERSION 12
 #if (NODE_MODULE_VERSION < NODE_0_12_MODULE_VERSION)
